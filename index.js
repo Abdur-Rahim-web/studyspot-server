8c579b6846e809855c0567006b7e1370b1bd8903
Abdur-Rahim-web <abdurrahimar04@gmail.com>
2026-05-31 16:17:39 +0600

    added delete api for DeleteRoom component

--- index.js
+++ index.js
@@ -69,6 +69,12 @@ async function run() {
             res.send(result);
         });
 
+        app.delete('/rooms/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await roomsCollection.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
