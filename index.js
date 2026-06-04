d325cdc6e8bc8e821a692a33bff62be7bec58419
Abdur-Rahim-web <abdurrahimar04@gmail.com>
2026-06-04 20:08:59 +0600

    Implement JWT with HTTP-Only Cookies for Add Room, Book Room, My Bookings, Edit/Delete, Cancel

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId, } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,6 +21,30 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+const JWKS = createRemoteJWKSet(new URL("http://localhost:3000/api/auth/jwks"));
+
+const verifyJWT = async (req, res, next) => {
+    const authHeader = req?.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Please log in first' });
+    }
+
+    const token = authHeader?.split(' ')[1];
+    if (!token) {
+        return res.status(401).send({ message: 'Token not found' });
+    }
+
+    try {
+        const { payload } = await jwtVerify(token, JWKS);
+        console.log(payload);
+        next();
+    } catch (error) {
+        return res.status(403).send({ message: 'Forbidden' });
+    }
+};
+
+
 
 async function run() {
     try {
@@ -35,7 +60,7 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/rooms/:id', async (req, res) => {
+        app.get('/rooms/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await roomsCollection.findOne(query);
@@ -54,13 +79,13 @@ async function run() {
 
         });
 
-        app.post('/rooms', async (req, res) => {
+        app.post('/rooms', verifyJWT, async (req, res) => {
             const newRoom = req.body;
             const result = await roomsCollection.insertOne(newRoom);
             res.send(result);
         });
 
-        app.patch('/rooms/:id', async (req, res) => {
+        app.patch('/rooms/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const updatedRoom = req.body;
             const result = await roomsCollection.updateOne(
@@ -70,7 +95,7 @@ async function run() {
             res.send(result);
         });
 
-        app.delete('/rooms/:id', async (req, res) => {
+        app.delete('/rooms/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const result = await roomsCollection.deleteOne({ _id: new ObjectId(id) });
             res.send(result);
@@ -79,19 +104,19 @@ async function run() {
         // Bookings API
         const bookingsCollection = database.collection('bookings');
 
-        app.post('/bookings', async (req, res) => {
+        app.post('/bookings', verifyJWT, async (req, res) => {
             const newBooking = req.body;
             const result = await bookingsCollection.insertOne(newBooking);
             res.send(result);
         });
 
-        app.get('/bookings/:userId', async (req, res) => {
+        app.get('/bookings/:userId', verifyJWT, async (req, res) => {
             const userId = req.params.userId;
             const result = await bookingsCollection.find({ userId: userId }).toArray();
             res.send(result);
         });
 
-        app.delete('/bookings/:bookingId', async (req, res) => {
+        app.delete('/bookings/:bookingId', verifyJWT, async (req, res) => {
             const bookingId = req.params.bookingId;
             const result = await bookingsCollection.deleteOne({ _id: new ObjectId(bookingId) });
             res.send(result);
